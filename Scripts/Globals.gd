2c2368d01ab8b97a423f4f6e58035c8573d3c990
unknown <ericzhen1978@gmail.com>
2023-07-19 16:44:26 -0400

    bad2

--- Scripts/Globals.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-
-var health = 100
-var debt = 9999999999
--- Scripts/Load Scene.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-@export var scenePath: PackedScene
-
-func _on_body_entered(body):
-	if body.name == "Player":
-		get_tree().unload_current_scene()
-		get_tree().change_scene_to_packed(scenePath)
-	
